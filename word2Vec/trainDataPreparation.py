ae775a7c72c21f5fb2c02f921d690b572405d82b
wangjunjieISCAS <wangjunjie012@sina.com>
2017-09-20 14:44:43 -0400

    duplicate detection and performance evaluation, as well as refactor the testDataPreparation and related

--- word2Vec/trainDataPreparation.py
+++ word2Vec/trainDataPreparation.py
@@ -8,109 +8,27 @@ Created on Tue Jul 25 17:10:16 2017
 #autoreload 2
 # ipython
 
-import word2vec
-import codecs
-from sentence2vec import Sentence2Vec
 import numpy as np
-
-class Word:
-    def __init__(self, text, vector):
-        self.text = text
-        self.vector = vector
+from multiModelWordEmbed import *
 
 
-# a sentence, a list of words
-class Sentence:
-    def __init__(self, word_list, word_frequency):
-        self.word_list = word_list
-        self.word_frequency = word_frequency
+doc2vec = Document2Vec() 
 
-    # return the length of a sentence
-    def len(self):
-        return len(self.word_list)
-    
-class Document2Vec:
-    def train_model(self):
-        word2vec.word2vec('data/train.txt', 'data/word2vec.bin', size=200, verbose=True)
-        
-    def get_model(self):
-        return word2vec.load('data/word2vec.bin')
-  
-    def predict_word2vec(self, word, model):
-        if word in model.vocab:
-            return model[word]
-        else:
-            return None
-    
-    def obtain_sentence_list (self):
-        sentence_list = []
-        model = self.get_model()
-        
-        file = codecs.open('data/multiTrainText.txt','r', 'utf-8')
-        content = file.readlines()
-        file.close()
+word2vec_train_file = 'data/train.txt'
+word2vec_model_file = 'data/word2vec.bin'
+word_vector_input_file = 'data/multiTrainText.txt'
 
-        token_num = 0
-        unfind_token_num = 0
-        for line in content:
-            line = line.replace('\r\n', '')
-            print line
-            tokens = line.split(' ')
-            
-            # find the no duplicate word in the sentence, and 
-            no_dup_tokens = []
-            tf_tokens = []
-            for word in tokens:
-                #print "word -1 :" + word
-                if word in no_dup_tokens:
-                    index = no_dup_tokens.index(word )
-                    tf_tokens[index] += 1
-                else:
-                    no_dup_tokens.append ( word)
-                    tf_tokens.append (1)
-            
-            sentence_words = []
-            for word in no_dup_tokens:
-                #print "word: " + word
-                #new_word = word.encode('utf-8', 'ignore').decode('utf-8')
-                vector = self.predict_word2vec(word, model)
-                if ( not (vector is None) ) :                    
-                    word_vector = Word( word, vector )
-                    sentence_words.append (word_vector)
-                    token_num += 1
-                else:
-                    unfind_token_num += 1
-            
-            sentence = Sentence (sentence_words, tf_tokens)
-            sentence_list.append (sentence)
-        
-        print token_num
-        print unfind_token_num
-        
-        return sentence_list
-    
-    def predict_sentence2vec(self, sentence_list, embedding_size):
-        sen2vec = Sentence2Vec()
-        sentence_vecs = sen2vec.sentence_to_vec(sentence_list, embedding_size)
-        return sentence_vecs   
-    
-    def predict_text2vec_obtain_text_source (self):        
-        sentence_list = self.obtain_sentence_list( )
-        sentence_vecs, zero_len_list = self.predict_sentence2vec(sentence_list, 200)
-        np.savetxt('data/textInput.txt', sentence_vecs)
-        np.save('data/zeroLenList.npy', zero_len_list)
-        return zero_len_list
-    
+word_embedding_out_file = 'data/trainTextInput.txt'
+word_embedding_zero_line_file = 'data/zeroLenListTest.npy'
 
-doc2vec = Document2Vec()   
-zero_len_list = doc2vec.predict_text2vec_obtain_text_source()
-temp = np.load('data/zeroLenList.npy')
-print temp
+word_embedding_total_out_file = 'data/trainTextInputTotal.txt'
 
-#doc2vec = Document2Vec()           
-#sentence_list = doc2vec.obtain_sentence_list( )
-#sentence_vecs, zero_len_list = doc2vec.predict_sentence2vec(sentence_list, 200)
-#np.savetxt('data/textInput.txt', sentence_vecs)
-#print zero_len_list
-#print len(sentence_vecs)
+embedding_size = 200
 
+zero_len_list = doc2vec.predict_text2vec_obtain_text_source( embedding_size, word2vec_train_file, word2vec_model_file, word_vector_input_file, word_embedding_out_file, word_embedding_zero_line_file )
+
+#vectorTool = VectorTransfer()
+#vectorTool.transfer_vector( embedding_size, word_embedding_out_file, word_embedding_zero_line_file, word_embedding_total_out_file )
+
+#temp = np.load ( word_embedding_zero_line_file)
+#print temp
--- word2Vec/vectorTransfer.py
+++ word2Vec/vectorTransfer.py
@@ -6,31 +6,44 @@ Created on Wed Aug 30 15:29:23 2017
 """
 
 import numpy as np
-
+ 
+# combine the testText file and zeroLenListTest together, use the default vector to represent the null vector 
 class VectorTransfer:
-    def transfer_vector (self, embedding_size):
+    def transfer_vector (self, embedding_size, word_embedding_out_file, word_embedding_zero_line_file, word_embedding_total_out_file ):
         text_vectors = []
+       
+        empty_vector = np.zeros(( embedding_size,) )
+        print empty_vector.dtype
         
-        empty_vector = []
-        for i in range(embedding_size):
-            empty_vector.append (0.00)
-        
-        zero_list = np.load('data/zeroLenListTest.npy')
+        zero_list = np.load( word_embedding_zero_line_file )
         print zero_list
         
-        text = np.loadtxt('data/testText.txt')
+        text = np.loadtxt( word_embedding_out_file )
+        print text.dtype
         
         total_size = len(text) + len(zero_list)
         index_text = 0
         for i in range(total_size):
             if i in zero_list:
                 text_vectors.append ( empty_vector )
+                #print "null"
             else:
-                text_vectors.append (text[index_text] ) 
+                text_vectors.append ( text[index_text] ) 
+                
                 index_text = index_text +1
         
-        np.savetxt('data/testTextTotal.txt', text_vectors)
+        print text_vectors
+        np.savetxt ( word_embedding_total_out_file, text_vectors)
 
-vectorTool = VectorTransfer()
-embedding_size = 100
-vectorTool.transfer_vector( embedding_size )
+#vectorTool = VectorTransfer()
+
+#embedding_size = 100
+#word_embedding_out_file = 'data/testText.txt'
+#word_embedding_zero_line_file = 'data/zeroLenListTest.npy'
+
+#word_embedding_total_out_file = 'data/testTextTotal.npy'
+
+#vectorTool.transfer_vector( embedding_size , word_embedding_out_file, word_embedding_zero_line_file, word_embedding_total_out_file)
+
+#array = np.load('data/testTextTotal.npy' )
+#print array
